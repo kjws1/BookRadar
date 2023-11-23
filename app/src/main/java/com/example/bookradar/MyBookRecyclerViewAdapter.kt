0c2b0457cfc6b973c1045e83355fb71c85c03b7f
larpios <larpios@protonmail.com>
2023-11-24 05:19:24 +0900

    Update MyBookRecyclerViewAdapter.kt

--- app/src/main/java/com/example/bookradar/MyBookRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/bookradar/MyBookRecyclerViewAdapter.kt
@@ -1,21 +1,18 @@
 package com.example.bookradar
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-
-import com.example.bookradar.placeholder.PlaceholderContent.PlaceholderItem
+import androidx.recyclerview.widget.RecyclerView
 import com.example.bookradar.databinding.FragmentItemBinding
 
-/**
- * [RecyclerView.Adapter] that can display a [PlaceholderItem].
- * TODO: Replace the implementation with code for your data type.
- */
 class MyBookRecyclerViewAdapter(
-    private val values: List<PlaceholderItem>
+    private var values: MutableList<BookInfo>
 ) : RecyclerView.Adapter<MyBookRecyclerViewAdapter.ViewHolder>() {
+
+    fun updateData(newData: List<BookInfo>){
+        values = newData.toMutableList()
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
@@ -31,25 +28,28 @@ class MyBookRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-/*
-        holder.idView.text = item.id
-        holder.contentView.text = item.content
-*/
-
-        holder.titleView.text =
+        /*
+                holder.idView.text = item.id
+                holder.contentView.text = item.content
+        */
+        holder.titleView.text = item.book!!.title
+        holder.authorView.text = item.book!!.author
+        holder.publisherView.text = item.book!!.publisher
+        holder.availabilityView.text = if (item.availability) "대출가능" else "대출불가능"
+        holder.coverImage.setImageBitmap(item.image)
     }
 
     override fun getItemCount(): Int = values.size
 
     inner class ViewHolder(binding: FragmentItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        val titleView = binding.textViewTitle
-        val authorView = binding.textViewAuthor
-        val publisherView = binding.textViewPublisher
-        val availabilityView = binding.textViewAvailability
-
-        override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
-        }
+        val titleView = binding.textTitle
+        val authorView = binding.textAuthor
+        val publisherView = binding.textPublisher
+        val availabilityView = binding.textAvailability
+        val isbnView = binding.textISBN
+        val coverImage = binding.imageBookCover
     }
+
+    // [Copied]: https://stackoverflow.com/questions/18953632/how-to-set-image-from-url-for-imageview
 
 }
