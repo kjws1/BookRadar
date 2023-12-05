ade24bf338fd16ef7acd4fe9a2ae21d105fbd005
larpios <larpios@protonmail.com>
2023-12-06 03:33:19 +0900

    fix: Fix navigation from BorrowFragment to HomeFragment not working when gone through MapsFragment

--- app/src/main/java/com/example/bookradar/MapsFragment.kt
+++ app/src/main/java/com/example/bookradar/MapsFragment.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.bookradar.databinding.BottomSheetBinding
@@ -80,8 +79,7 @@ class ModalBottomSheet : BottomSheetDialogFragment() {
         binding.textLibraryLocation.text = book.loc
         binding.buttonBorrow.setOnClickListener {
             MapsFragmentDirections.actionNavMapsToNavBorrow(book).let {
-                (requireActivity() as AppCompatActivity).supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main)!!
-                    .findNavController().navigate(it)
+                findNavController().navigate(it)
                 dismiss()
             }
         }
